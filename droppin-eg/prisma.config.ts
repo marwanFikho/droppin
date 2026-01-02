05c5388f112f126c4364617095b9dc0385b03979
marwanFikho <marwan123hesham123@gmail.com>
2026-01-03 01:00:53 +0200

    Latest

--- droppin-eg/prisma.config.ts
+++ droppin-eg/prisma.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from "@prisma/internals";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
+export default {
   datasources: {
     db: {
-      url: `file:${path.join(__dirname, "prisma/dev.sqlite")}`,
+      url: `file:${path.resolve(__dirname, "prisma/dev.sqlite")}`,
     },
   },
-});
+};
