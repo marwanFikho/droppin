cb51f3887914c6d22df908361f764cbe61c1c5ef
marwanFikho <marwan123hesham123@gmail.com>
2026-01-03 01:04:54 +0200

    Latest

--- droppin-eg/app/db.server.js
+++ droppin-eg/app/db.server.js
@@ -1,30 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const databaseUrl = `file:${path.join(__dirname, "../prisma/dev.sqlite")}`;
 
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient({
-      datasources: {
-        db: {
-          url: databaseUrl,
-        },
-      },
-    });
+    global.prismaGlobal = new PrismaClient();
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: databaseUrl,
-    },
-  },
-});
+const prisma = global.prismaGlobal ?? new PrismaClient();
 
 export default prisma;
